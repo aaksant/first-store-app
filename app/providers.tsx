0f37183d70053d368e5a4ca8b2441a1e92f89abb
aaksant <aaksangit@gmail.com>
2025-06-29 21:30:43 +0700

    feat: setup toaster

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,18 +2,22 @@
 
 import { ThemeProvider } from '@/components/theme-provider';
 import { ReactNode } from 'react';
+import { Toaster } from 'sonner';
 
 type ProvidersProps = { children: ReactNode };
 
 export default function Providers({ children }: ProvidersProps) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+      <Toaster />
+    </>
   );
 }
