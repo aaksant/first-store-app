fade8ba3dc9e879c05b0755a77814fc186824dfd
aaksant <aaksangit@gmail.com>
2025-06-29 21:13:52 +0700

    refactor: add `Promise` generic in `searchParams` parameter

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,17 +1,12 @@
 import ProductsContainer from '@/components/products/products-container';
 
-type ProductsPageProps = {
-  searchParams: {
-    layout?: string;
-    search?: string;
-  };
-};
-
 export default async function ProductsPage({
   searchParams
-}: ProductsPageProps) {
-  const layout = searchParams.layout || 'grid';
-  const search = searchParams.search || '';
+}: {
+  searchParams: Promise<{ layout?: string; search?: string }>;
+}) {
+  const layout = (await searchParams).layout || 'grid';
+  const search = (await searchParams).search || '';
 
   return (
     <>
