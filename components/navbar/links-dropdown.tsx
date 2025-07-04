8592914e35b57d5a89b46af85da3de84c32bfb92
aaksant <aaksangit@gmail.com>
2025-07-04 23:06:09 +0700

    feat: add access restriction for non-admin users to see dashboard link in routes navigation

--- components/navbar/links-dropdown.tsx
+++ components/navbar/links-dropdown.tsx
@@ -12,8 +12,12 @@ import { AlignJustify } from 'lucide-react';
 import SignOutLink from './sign-out-link';
 import UserIcon from './user-icon';
 import { SignedIn, SignedOut, SignInButton } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
 
-export default function LinksDropdown() {
+export default async function LinksDropdown() {
+  const { userId } = await auth();
+  const isAdminUser = userId === process.env.ADMIN_USER_ID;
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -24,13 +28,18 @@ export default function LinksDropdown() {
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <SignedIn>
-          {navLinks.map((navLink) => (
-            <DropdownMenuItem key={navLink.href}>
-              <Link href={navLink.href} className="w-full">
-                {navLink.label}
-              </Link>
-            </DropdownMenuItem>
-          ))}
+          {navLinks.map((navLink) => {
+            if (navLink.label === 'dasboard' && !isAdminUser) {
+              return null;
+            }
+            return (
+              <DropdownMenuItem key={navLink.href}>
+                <Link href={navLink.href} className="w-full">
+                  {navLink.label}
+                </Link>
+              </DropdownMenuItem>
+            );
+          })}
           <DropdownMenuSeparator />
           <DropdownMenuItem>
             <SignOutLink />
