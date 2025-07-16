44a698a9d1e81a58cfc862ecefd71be46295d9f2
aaksant <aaksangit@gmail.com>
2025-07-16 23:00:12 +0700

    feat: implement getters for action success or error message

--- db/helpers.ts
+++ db/helpers.ts
@@ -1,6 +1,8 @@
 'use server';
 
+import { ActionStatus } from '@/utils/types';
 import { currentUser, User } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
 export async function getAuthUser(): Promise<User> {
   const user = await currentUser();
@@ -11,10 +13,27 @@ export async function getAuthUser(): Promise<User> {
   return user;
 }
 
-export async function getCreateProductActionError(
-  error: unknown
-): Promise<{ message: string }> {
+export async function getAdminUser() {
+  const user = await getAuthUser();
+  if (user.id !== process.env.ADMIN_USER_ID) redirect('/');
+
+  return user;
+}
+
+export async function getActionSuccessMessage(
+  successMessage: string
+): Promise<{ status: ActionStatus; message: string }> {
   return {
-    message: error instanceof Error ? error.message : 'An error occured'
+    status: 'success',
+    message: successMessage
   };
+}
+
+export async function getActionErrorMessage(
+  error: unknown
+): Promise<{ status: ActionStatus; message: string }> {
+  const errorMessage =
+    error instanceof Error ? error.message : 'An error occured';
+
+  return { status: 'error', message: errorMessage };
 }
