9d07860b2a0aa7322d73bd9bd6115ecda0ceb43f
aaksant <aaksangit@gmail.com>
2025-07-26 21:43:54 +0700

    feat: implement favorite page

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,3 +1,26 @@
-export default function FavoritesPage() {
-  return <div>FavoritesPage</div>;
+import LoadingContainer from '@/components/globals/loading-container';
+import SectionTitle from '@/components/globals/section-title';
+import NoProductFound from '@/components/products/no-product-found';
+import ProductsGrid from '@/components/products/products-grid';
+import { getFavoriteProducts } from '@/db/actions';
+import { Suspense } from 'react';
+
+export default async function FavoritesPage() {
+  const favoriteProducts = (await getFavoriteProducts()).map(
+    (favoriteProduct) => favoriteProduct.product
+  );
+  const totalFavorites = favoriteProducts.length;
+
+  return (
+    <>
+      <SectionTitle text="Favorites" />
+      <Suspense fallback={<LoadingContainer />}>
+        {totalFavorites === 0 ? (
+          <NoProductFound text="You have no favorites" />
+        ) : (
+          <ProductsGrid products={favoriteProducts} />
+        )}
+      </Suspense>
+    </>
+  );
 }
