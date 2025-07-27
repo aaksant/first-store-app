9fc2f34d94f6c4ff0a7d12bf11ac6521d53ae25c
aaksant <aaksangit@gmail.com>
2025-07-27 20:06:52 +0700

    chore: rename component name

--- components/globals/loading-product-card.tsx
+++ components/globals/loading-product-card.tsx
@@ -18,7 +18,7 @@ function ProductCardSkeleton() {
   );
 }
 
-export default function LoadingContainer() {
+export default function LoadingProductCard() {
   return (
     <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       <ProductCardSkeleton />
