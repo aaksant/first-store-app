7f0a8cb5992838bba46da8bdb36d2cac0c54d32a
aaksant <aaksangit@gmail.com>
2025-07-30 21:21:44 +0700

    feat: better database seeding with FakeStoreAPI and `faker`

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,44 @@
 import { PrismaClient } from '@prisma/client';
-import { products } from './products';
+import { DummyProduct } from '@/utils/types';
+import { faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient();
 
+async function getSampleProducts(): Promise<DummyProduct[] | null> {
+  try {
+    const response = await fetch('https://fakestoreapi.com/products');
+    if (!response.ok) {
+      throw new Error(`Failed to fetch products: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
 async function main() {
-  for (const product of products) {
+  await prisma.review.deleteMany({});
+  await prisma.favoriteProduct.deleteMany({});
+  await prisma.product.deleteMany({});
+
+  const sampleProducts = await getSampleProducts();
+  if (!sampleProducts) {
+    console.log('Could not fetch sample products. Exiting seed process.');
+    return;
+  }
+
+  for (const sampleProduct of sampleProducts) {
     await prisma.product.create({
-      data: product
+      data: {
+        name: sampleProduct.title,
+        company: `${faker.commerce.productAdjective()} ${faker.commerce.productMaterial()}`,
+        description: sampleProduct.description,
+        featured: Math.random() < 0.5,
+        image: sampleProduct.image,
+        price: Math.round(sampleProduct.price),
+        clerkId: 'clerkId'
+      }
     });
   }
 }
