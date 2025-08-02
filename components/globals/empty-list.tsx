07b6187fe2c916729cffb1a861688a798638ac34
aaksant <aaksangit@gmail.com>
2025-08-02 20:09:47 +0700

    chore: add default `className` value

--- components/globals/empty-list.tsx
+++ components/globals/empty-list.tsx
@@ -9,5 +9,14 @@ export default function EmptyList({
   text = 'No items found.',
   className
 }: EmptyListProps) {
-  return <h2 className={cn('text-xl tracking-tight', className)}>{text}</h2>;
+  return (
+    <h2
+      className={cn(
+        'text-xl tracking-tight text-center text-muted-foreground tracking-tight font-semibold',
+        className
+      )}
+    >
+      {text}
+    </h2>
+  );
 }
