21d5420a9deca4a1c5b5a250d709b01b827bac55
aaksant <aaksangit@gmail.com>
2025-08-03 17:24:17 +0700

    chore: overwrite `TableCaption` `text-muted-foreground` class to `text-foreground` for better readability

--- components/admin/admin-products-table.tsx
+++ components/admin/admin-products-table.tsx
@@ -98,7 +98,7 @@ export default async function AdminProductsTable({
               </TableRow>
             ))}
           </TableBody>
-          <TableCaption>
+          <TableCaption className="text-foreground">
             <PaginationContainer
               currentPage={currentPage}
               totalPages={totalPages}
