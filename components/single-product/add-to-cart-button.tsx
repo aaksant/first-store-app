e54443b8d13196978e57e990e9a7c5ca0979d646
aaksant <aaksangit@gmail.com>
2025-08-05 22:25:00 +0700

    feat: disable add to cart functionality to unauthenticated user

--- components/single-product/add-to-cart-button.tsx
+++ components/single-product/add-to-cart-button.tsx
@@ -1,5 +1,11 @@
+import { auth } from '@clerk/nextjs/server';
 import { Button } from '../ui/button';
+import ProtectedAddToCartButton from './protected-add-to-cart-button';
 
-export default function AddToCartButton() {
+export default async function AddToCartButton() {
+  const { userId } = await auth();
+
+  if (!userId) return <ProtectedAddToCartButton />;
+
   return <Button size="default">Add to cart</Button>;
 }
