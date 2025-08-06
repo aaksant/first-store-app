21ae2a1aa95f8d83b9e503d931424f6725c29c0c
aaksant <aaksangit@gmail.com>
2025-08-06 22:45:45 +0700

    chore: add missing `btn` class

--- components/single-product/add-to-cart-button.tsx
+++ components/single-product/add-to-cart-button.tsx
@@ -2,6 +2,7 @@ import { auth } from '@clerk/nextjs/server';
 import { Button } from '../ui/button';
 import { ShoppingCart } from 'lucide-react';
 import ProtectedAddToCartButton from './protected-add-to-cart-button';
+import { cn } from '@/lib/utils';
 
 type AddToCartButtonProps = {
   className?: string;
@@ -15,7 +16,7 @@ export default async function AddToCartButton({
   if (!userId) return <ProtectedAddToCartButton />;
 
   return (
-    <Button size="default" className={className}>
+    <Button size="default" className={cn('btn', className)}>
       <ShoppingCart />
       Add to cart
     </Button>
