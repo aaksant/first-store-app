fc7bf48a755ebb9441b4372a6e99195464e6ce2a
aaksant <aaksangit@gmail.com>
2025-08-18 21:55:33 +0700

    refactor: wrap the component in `ActionButton`

--- components/single-product/add-to-cart-button.tsx
+++ components/single-product/add-to-cart-button.tsx
@@ -1,8 +1,7 @@
 import { auth } from '@clerk/nextjs/server';
-import { Button } from '../ui/button';
 import { ShoppingCart } from 'lucide-react';
 import ProtectedAddToCartButton from './protected-add-to-cart-button';
-import { cn } from '@/lib/utils';
+import ActionButton from '../form/action-button';
 
 type AddToCartButtonProps = {
   className?: string;
@@ -15,10 +14,5 @@ export default async function AddToCartButton({
 
   if (!userId) return <ProtectedAddToCartButton className={className} />;
 
-  return (
-    <Button size="default" className={cn('btn', className)}>
-      <ShoppingCart />
-      Add to cart
-    </Button>
-  );
+  return <ActionButton icon={<ShoppingCart />} className={className} />;
 }
